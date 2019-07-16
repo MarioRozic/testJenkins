467e31a408289f96147b957bd708fa03657eb27d
Mario <rozic.mario@hotmail.com>
2019-07-16 21:05:14 +0200

    init 14

--- members.js
+++ members.js
@@ -1,7 +1,7 @@
 
 const members = [
     {id: 1, name: 'John Doe', email: 'john@MediaList.com', status: "active"},
-    {id: 2, name: 'John Doe', email: 'john@MediaList.com', status: "jos jednom"},
+    {id: 2, name: 'John Doe', email: 'john@MediaList.com', status: "jos jednom 1"},
     {id: 3, name: 'John Doe', email: 'john@MediaList.com', status: "active"},
     {id: 4, name: 'John Doe', email: 'john@MediaList.com', status: "active"},
     {id: 5, name: 'John Doe', email: 'john@MediaList.com', status: "active"},
